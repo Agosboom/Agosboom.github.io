262105b93155987a8b73cd48f8780bda6a6b3851
Agostina Heinzmann <111083972+Agosboom@users.noreply.github.com>
2023-08-29 10:58:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtenemos todos los elementos con la clase "nav-link"
+    
     const navLinks = document.querySelectorAll(".nav-link");
 
-    // Agregamos el manejador de eventos a cada enlace
+
     navLinks.forEach((link) => {
         link.addEventListener("click", function (event) {
             event.preventDefault();
             const target = this.getAttribute("data-target");
             const section = document.getElementById(target);
 
-            // Hacemos scroll hacia la sección correspondiente
+            
             if (section) {
                 section.scrollIntoView({ behavior: "smooth" });
             }
